ae710d9c3d38a7760d0003dda683cf119e985abd
9yu <9.yubako@gmail.com>
2017-08-13 22:36:56 +0800

    2236

--- run-ab.bat
+++
@@ -1,2 +0,0 @@
-set current=D:/Sync/Local/I/9yu-web/git/gfwlist-p/
-gfwlist2pac -i %current%gfwlist/gfwlist.txt -f %current%gfwlist_p_ab.pac --user-rule %current%custom.txt -p "PROXY 127.0.0.1:1080;" --precise
